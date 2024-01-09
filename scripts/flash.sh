7062cebe9b094c5c85e7562e6d1d523e0aa82883
xqft <estefano.bargas@fing.edu.uy>
2024-01-09 01:31:42 +0000

    MCPWN

--- scripts/flash.sh
+++ scripts/flash.sh
@@ -18,4 +18,4 @@ case "$1" in
     ;;
 esac
 
-web-flash --chip esp32 target/xtensa-esp32-none-elf/${BUILD_MODE}/esp32_shenanigans
+web-flash --chip esp32 target/xtensa-esp32-none-elf/${BUILD_MODE}/esp32-shenanigans
--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,11 @@ use hal::{
     clock::ClockControl,
     embassy::executor::Executor,
     gpio::{AnyPin, Output, PushPull},
+    mcpwm::{
+        operator::{PwmActions, PwmPinConfig, PwmUpdateMethod},
+        timer::PwmWorkingMode,
+        PeripheralClockConfig, MCPWM,
+    },
     peripherals::Peripherals,
     prelude::*,
     Delay, IO,
@@ -42,6 +47,8 @@ async fn handle_led(mut led_pin: AnyPin<Output<PushPull>>) {
     }
 }
 
+//async fn handle_motor(mut motor_pin: )
+
 #[main]
 async fn main(_spawner: Spawner) {
     init_heap();
@@ -49,16 +56,38 @@ async fn main(_spawner: Spawner) {
     let system = peripherals.SYSTEM.split();
     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
 
+    // setup timer0
     let clocks = ClockControl::max(system.clock_control).freeze();
+    let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks);
+    embassy::init(&clocks, timer_group0.timer0);
+
+    let clock_cfg = PeripheralClockConfig::with_frequency(&clocks, 40u32.MHz()).unwrap();
+
+    // motor pwm
+    let mut mcpwm = MCPWM::new(peripherals.MCPWM0, clock_cfg);
+    mcpwm.operator0.set_timer(&mcpwm.timer0);
+    let mut pwm_pin = mcpwm.operator0.with_pin_a(
+        io.pins.gpio32.into_push_pull_output(),
+        PwmPinConfig::new(PwmActions::UP_ACTIVE_HIGH, PwmUpdateMethod::SYNC_IMMEDIATLY),
+    );
+    let timer_clock_cfg = clock_cfg
+        .timer_clock_with_frequency(99, PwmWorkingMode::Increase, 5u32.kHz())
+        .unwrap();
+    mcpwm.timer0.start(timer_clock_cfg);
+
+    for i in 0..5 {
+        pwm_pin.set_timestamp(99 / 5 * i);
+        Timer::after_secs(1).await;
+    }
+    for i in (0..5).rev() {
+        pwm_pin.set_timestamp(99 / 5 * i);
+        Timer::after_secs(1).await;
+    }
 
     // setup logger
     esp_println::logger::init_logger_from_env();
     log::info!("Logger is setup");
 
-    let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks);
-    embassy::init(&clocks, timer_group0.timer0);
-
     let led_pin = io.pins.gpio2.into_push_pull_output().into();
-
     handle_led(led_pin).await;
 }
